9a5a753d536f0f49a661112eca3308edeb0a0dcd
EC2 Default User <ec2-user@ip-172-31-28-12.eu-west-3.compute.internal>
2022-08-15 11:30:35 +0000

    add files

---
+++ script.sh
@@ -0,0 +1,6 @@
+aws autoscaling complete-lifecycle-action \
+          --lifecycle-hook-name <value>
+          --auto-scaling-group-name <value>
+          --lifecycle-action-result CONTINUE
+          --instance-id <ID_Instance>
+          --region eu-west-3
